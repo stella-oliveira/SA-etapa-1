0c55dfd1ba66c2b00eff0cf1929045bc74b793a3
stella-oliveira <74307418+stella-oliveira@users.noreply.github.com>
2020-11-11 10:59:38 -0300

    Update Stella - SA LP - Código do Fluxograma 1 da Etapa 1.c

--- Stella - SA LP - Código do Fluxograma 1 da Etapa 1.c
+++ Stella - SA LP - Código do Fluxograma 1 da Etapa 1.c
@@ -1,12 +1,12 @@
 /* Algoritmo por: STELLA OLIVEIRA E SILVA
 SENAI Centro - Turma: TII2001M
-Situação de Aprendizagem da Unidade Curricular "Lógica de Programação"
+SituaÃ§Ã£o de Aprendizagem da Unidade Curricular "LÃ³gica de ProgramaÃ§Ã£o"
  
-Descrição: algoritmo para uma escola de inglês, que lê o nome e as 4 notas de um aluno, 
-calcula e imprime o total e a média das notas. */
+DescriÃ§Ã£o: algoritmo para uma escola de inglÃªs, que lÃª o nome e as 4 notas de um aluno, 
+calcula e imprime o total e a mÃ©dia das notas. */
 
 #include <stdio.h>
-#include <stdlib.h> // system("cls")
+#include <stdlib.h> 	// system("cls")
 #include <conio.h>	// getch()
 #include "BibliotecaViana.c"
 
@@ -17,44 +17,44 @@ void cabecalho(){
 	cor(244); corFundo(244);
 	int i=0;
 	
-	gotoxy(1,1); printf("%c", 201);		// Canto superior esquerdo do cabeçalho
+	gotoxy(1,1); printf("%c", 201);				// Canto superior esquerdo do cabeÃ§alho
 	for(i=0;i<66;i++){
-		printf("%c", 205);				// Traço horizontal do cabeçalho
+		printf("%c", 205);				// TraÃ§o horizontal do cabeÃ§alho
 	}
-	printf("%c", 187);					// Canto superior direito do cabeçalho
+	printf("%c", 187);					// Canto superior direito do cabeÃ§alho
 	
-	printf("\n%c", 186);				// Traço vertical esquerdo do cabeçalho
-	gotoxy(2,23); printf("ESCOLA DE INGL%cS ATHENA", 210);	// CABEÇALHO
-	gotoxy(2,68); printf("%c", 186);	// Traço vertical direito do cabeçalho
+	printf("\n%c", 186);					// TraÃ§o vertical esquerdo do cabeÃ§alho
+	gotoxy(2,23); printf("ESCOLA DE INGL%cS ATHENA", 210);	// CABEÃ‡ALHO
+	gotoxy(2,68); printf("%c", 186);			// TraÃ§o vertical direito do cabeÃ§alho
 	
-	printf("\n%c", 200);				// Canto inferior esquerdo do cabeçalho
+	printf("\n%c", 200);					// Canto inferior esquerdo do cabeÃ§alho
 	for(i=0;i<66;i++){
-		printf("%c", 205);				// Traço horizontal do cabeçalho
+		printf("%c", 205);				// TraÃ§o horizontal do cabeÃ§alho
 	}
-	gotoxy(3,68); printf("%c", 188);	// Canto inferior direito do cabeçalho
+	gotoxy(3,68); printf("%c", 188);			// Canto inferior direito do cabeÃ§alho
 }
 
 void tabelaDados(){
 	cabecalho();
 	int i=0;
 	
-	gotoxy(4,1); printf("%c", 218);		// Canto superior esquerdo da tabela de dados
+	gotoxy(4,1); printf("%c", 218);				// Canto superior esquerdo da tabela de dados
 	for(i=0; i<66; i++){
-		printf("%c", 196);				// Traço horizontal da tabela de dados
+		printf("%c", 196);				// TraÃ§o horizontal da tabela de dados
 	}
 	printf("%c", 191);					// Canto superior direito da tabela de dados
 	
-	printf("\n%c", 179);				// Traço vertical esquerdo da tabela de dados (linha 5)
+	printf("\n%c", 179);					// TraÃ§o vertical esquerdo da tabela de dados (linha 5)
 	gotoxy(5,68); printf("%c", 179);
 	
-	gotoxy(6,1); printf("%c", 195); 	// Traço vertical esquerdo com linha no meio
+	gotoxy(6,1); printf("%c", 195); 			// TraÃ§o vertical esquerdo com linha no meio
 	for(i=0; i<66; i++){
-		printf("%c", 196);				// Traço horizontal da tabela de dados
+		printf("%c", 196);				// TraÃ§o horizontal da tabela de dados
 	}
-	printf("%c", 180);					// Traço vertical direito com linha no meio
+	printf("%c", 180);					// TraÃ§o vertical direito com linha no meio
 	
-	printf("\n%c", 179);				// Traço vertical esquerdo da tabela de dados (linha 7)
-	gotoxy(6,18); printf("%c", 194);	// T para baixo
+	printf("\n%c", 179);					// TraÃ§o vertical esquerdo da tabela de dados (linha 7)
+	gotoxy(6,18); printf("%c", 194);			// T para baixo
 	gotoxy(6,35); printf("%c", 194);
 	gotoxy(6,51); printf("%c", 194);
 	gotoxy(7,18); printf("%c", 179);
@@ -62,13 +62,13 @@ void tabelaDados(){
 	gotoxy(7,51); printf("%c", 179);
 	gotoxy(7,68); printf("%c", 179);
 	
-	printf("\n%c", 192); 				// Canto inferior esquerdo da tabela de dados
+	printf("\n%c", 192); 					// Canto inferior esquerdo da tabela de dados
 	for(i=0; i<66; i++){
-		printf("%c", 196);				// Traço horizontal da tabela de dados
+		printf("%c", 196);				// TraÃ§o horizontal da tabela de dados
 	}
 	printf("%c", 217); 					// Canto inferior direito da tabela de dados
 	
-	gotoxy(8,18); printf("%c", 193);	// T para cima
+	gotoxy(8,18); printf("%c", 193);			// T para cima
 	gotoxy(8,35); printf("%c", 193);
 	gotoxy(8,51); printf("%c", 193);
 }
@@ -77,33 +77,33 @@ void tabelaResultado(){
 	cabecalho();
 	int i=0;
 	
-	gotoxy(4,1); printf("%c", 218);		// Canto superior esquerdo da tabela de dados
+	gotoxy(4,1); printf("%c", 218);				// Canto superior esquerdo da tabela de dados
 	for(i=0; i<66; i++){
-		printf("%c", 196);				// Traço horizontal da tabela de dados
+		printf("%c", 196);				// TraÃ§o horizontal da tabela de dados
 	}
 	printf("%c", 191);					// Canto superior direito da tabela de dados
 	
-	printf("\n%c", 179);				// Traço vertical esquerdo da tabela de dados (linha 5)
+	printf("\n%c", 179);					// TraÃ§o vertical esquerdo da tabela de dados (linha 5)
 	gotoxy(5,68); printf("%c", 179);
 	
-	gotoxy(6,1); printf("%c", 195); 	// Traço vertical esquerdo com linha no meio
+	gotoxy(6,1); printf("%c", 195); 			// TraÃ§o vertical esquerdo com linha no meio
 	for(i=0; i<66; i++){
-		printf("%c", 196);				// Traço horizontal da tabela de dados
+		printf("%c", 196);				// TraÃ§o horizontal da tabela de dados
 	}
-	printf("%c", 180);					// Traço vertical direito com linha no meio
+	printf("%c", 180);					// TraÃ§o vertical direito com linha no meio
 	
-	printf("\n%c", 179);				// Traço vertical esquerdo da tabela de dados (linha 7)
-	gotoxy(6,33); printf("%c", 194);	// T para baixo
+	printf("\n%c", 179);					// TraÃ§o vertical esquerdo da tabela de dados (linha 7)
+	gotoxy(6,33); printf("%c", 194);			// T para baixo
 	gotoxy(7,33); printf("%c", 179);
 	gotoxy(7,68); printf("%c", 179);
 	
-	printf("\n%c", 192); 				// Canto inferior esquerdo da tabela de dados
+	printf("\n%c", 192); 					// Canto inferior esquerdo da tabela de dados
 	for(i=0; i<66; i++){
-		printf("%c", 196);				// Traço horizontal da tabela de dados
+		printf("%c", 196);				// TraÃ§o horizontal da tabela de dados
 	}
 	printf("%c", 217); 					// Canto inferior direito da tabela de dados
 	
-	gotoxy(8,33); printf("%c", 193);	// T para cima
+	gotoxy(8,33); printf("%c", 193);			// T para cima
 }
 
 void dados(){
